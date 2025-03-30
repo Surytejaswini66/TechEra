9ca6dcc9d93480c368da6829791c6d65a96c123e
Surytejaswini66 <suryaviswanadhapalli666@gmail.com>
2025-03-30 17:39:10 +0530

    Initial commit

--- src/components/CourseItemDetails/index.js
+++ src/components/CourseItemDetails/index.js
@@ -1,23 +1,26 @@
-import React, { Component } from 'react';
-import { withRouter } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import Header from '../Header';
 import './index.css';
 
-class CourseItemDetails extends Component {
-  state = { isLoading: true, isFailed: false, isSuccess: false, ItemDetails: {} };
+const CourseItemDetails = () => {
+  const { id } = useParams(); // Get the course ID from the URL
+  const [state, setState] = useState({
+    isLoading: true,
+    isFailed: false,
+    isSuccess: false,
+    ItemDetails: {},
+  });
 
-  componentDidMount() {
-    this.fetchCourseDetails();
-  }
+  useEffect(() => {
+    fetchCourseDetails();
+  }, []); // Fetch data on component mount
 
-  fetchCourseDetails = async () => {
-    const { params } = this.props.match; // Now using `withRouter` to access match
-    const { id } = params;
-    
+  const fetchCourseDetails = async () => {
     try {
       const response = await fetch(`https://apis.ccbp.in/te/courses/${id}`);
       const data = await response.json();
-      
+
       if (response.ok) {
         const updatedData = {
           description: data.course_details.description,
@@ -25,53 +28,51 @@ class CourseItemDetails extends Component {
           name: data.course_details.name,
           imageUrl: data.course_details.image_url,
         };
-        this.setState({
+        setState({
           isLoading: false,
           isSuccess: true,
           isFailed: false,
           ItemDetails: updatedData,
         });
       } else {
-        this.setState({ isLoading: false, isSuccess: false, isFailed: true });
+        setState({ isLoading: false, isSuccess: false, isFailed: true });
       }
     } catch (error) {
-      this.setState({ isLoading: false, isSuccess: false, isFailed: true });
+      setState({ isLoading: false, isSuccess: false, isFailed: true });
     }
   };
 
-  render() {
-    const { isLoading, isFailed, isSuccess, ItemDetails } = this.state;
-    const { description, name, imageUrl } = ItemDetails;
+  const { isLoading, isFailed, isSuccess, ItemDetails } = state;
+  const { description, name, imageUrl } = ItemDetails;
 
-    return (
+  return (
+    <div>
+      <Header />
       <div>
-        <Header />
-        <div>
-          {isLoading && <p>Loading...</p>}
-          {isSuccess && (
-            <div className="itemDetails">
-              <img src={imageUrl} alt={name} />
-              <h1>{name}</h1>
-              <p>{description}</p>
-            </div>
-          )}
-          {isFailed && (
-            <div>
-              <img
-                src="https://assets.ccbp.in/frontend/react-js/tech-era/failure-img.png"
-                alt="failure view"
-              />
-              <h1>Oops! Something Went Wrong</h1>
-              <p>We cannot seem to find the page you are looking for</p>
-              <button type="button" onClick={this.fetchCourseDetails}>
-                Retry
-              </button>
-            </div>
-          )}
-        </div>
+        {isLoading && <p>Loading...</p>}
+        {isSuccess && (
+          <div className="itemDetails">
+            <img src={imageUrl} alt={name} />
+            <h1>{name}</h1>
+            <p>{description}</p>
+          </div>
+        )}
+        {isFailed && (
+          <div>
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/tech-era/failure-img.png"
+              alt="failure view"
+            />
+            <h1>Oops! Something Went Wrong</h1>
+            <p>We cannot seem to find the page you are looking for</p>
+            <button type="button" onClick={fetchCourseDetails}>
+              Retry
+            </button>
+          </div>
+        )}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
-export default withRouter(CourseItemDetails);
+export default CourseItemDetails;
